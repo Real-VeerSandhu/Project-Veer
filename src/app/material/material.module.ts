71821a81bbb3ee302fb302805a811133f40972d7
Veer Sandhu <sandhu.s.veer@gmail.com>
2021-09-25 23:45:13 -0400

    toolbar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatToolbarModule, MatInputModule, MatCardModule, MatTabsModule,
-  MatDialogModule, MatDividerModule, MatListModule, MatIconModule,  } from '@angular/material';
+  MatDialogModule, MatDividerModule, MatListModule, MatIconModule, MatMenuModule,  } from '@angular/material';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {MatRadioModule} from '@angular/material/radio';
@@ -21,7 +21,8 @@ const MaterialComponents = [
   DragDropModule,
   MatRadioModule,
   MatSnackBarModule,
-  MatSliderModule
+  MatSliderModule,
+  MatMenuModule
 ];
 
 @NgModule({
